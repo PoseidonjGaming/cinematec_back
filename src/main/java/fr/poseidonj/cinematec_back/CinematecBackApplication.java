9035b57d5b6228f3bad4d77d0d8f6b21c078f1e0
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2024-07-19 18:21:03 +0200

    Initial commit

---
+++ src/main/java/fr/poseidonj/cinematec_back/CinematecBackApplication.java
@@ -0,0 +1,13 @@
+package fr.poseidonj.cinematec_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematecBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematecBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/poseidonj/cinematec_back/CinematecBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.poseidonj.cinematec_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematecBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
