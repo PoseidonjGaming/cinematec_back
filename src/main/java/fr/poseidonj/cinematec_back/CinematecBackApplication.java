a61135dcb66fe6d52b91b9c8761920dca6ac7ed1
Lucas Magnier <73874300+PoseidonjGaming@users.noreply.github.com>
2024-07-20 18:58:18 +0200

    exclude security auto config

--- src/main/java/fr/poseidonj/cinematec_back/CinematecBackApplication.java
+++ src/main/java/fr/poseidonj/cinematec_back/CinematecBackApplication.java
@@ -2,12 +2,13 @@ package fr.poseidonj.cinematec_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CinematecBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinematecBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinematecBackApplication.class, args);
+    }
 
 }
